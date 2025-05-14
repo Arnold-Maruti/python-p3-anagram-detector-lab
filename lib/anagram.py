b244b18e87d8ba0a0bb2ad151b9ff60f65173414
Arnold <Arnold>
2025-05-14 11:03:31 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word
+
+    def match(self,word):
+        z=[]
+        for x in word:
+            word=[letter for letter in x]
+            if (sorted(word)==sorted([char for char in self.word])):
+                z.append(x)
+                
+            else:
+                print("")
+        return z
+
+arnold=Anagram("listen")
+
+print(arnold.match(["silent","enlist"]))
+   
